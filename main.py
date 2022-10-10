dae1b2f94ff4e832a43997272ff864cf8d332cdc
Louis King <jinglemansweep@gmail.com>
2022-10-10 21:52:12 +0100

    Stashing before I lose it again

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
-from ledmatrix import LedMatrix
+time.sleep(2)
+
+import gc
+import uasyncio as asyncio
+from lib.mqttas import MQTTClient, config
+from lib.ledmatrix import LedMatrix
+from lib.hal import HAL
 from pixelfont import PixelFont
-from hal import HAL
-from utils import wifi_connect, set_ntp_clock
-from secrets import WIFI_SSID, WIFI_KEY
+
+gc.collect()
 
 print("START")
-
-WLAN = "PT"
 
 GPIO_PIN = 16
 DISPLAY_PIXEL_COUNT = 256
@@ -17,11 +20,14 @@ DISPLAY_FPS = 10
 DISPLAY_DEBUG = False
 DISPLAY_INTENSITY = 2
 
-wifi_connect(WIFI_SSID, WIFI_KEY)
-set_ntp_clock()
+MQTT_TOPIC_PREFIX = 'mqttled/test'
+
+outages = 0
+
+#wifi_connect(WIFI_SSID, WIFI_KEY)
+#set_ntp_clock()
 
 driver = HAL(gpio_pin=GPIO_PIN, pixel_count=DISPLAY_PIXEL_COUNT)
-
 
 display = LedMatrix(driver, dict(
     debug=DISPLAY_DEBUG,
@@ -30,6 +36,61 @@ display = LedMatrix(driver, dict(
     fps=DISPLAY_FPS
 ))
 
+async def test_cb():
+    print('Test Callback')
+    await asyncio.sleep(0.2)
+
+async def echo(msg):
+    print(f'ECHO: {msg}')
+
+async def handle_wifi(state):
+    global outages
+    if state:
+        print('Status: Connected')
+    else:
+        outages += 1
+        print('Status: Not Connected')
+    await asyncio.sleep(1)
+
+async def handle_connection(client):
+    await client.subscribe(f'{MQTT_TOPIC_PREFIX}/#', 1)
+
+def on_message(_topic, _msg, retained):
+    topic = _topic.decode()
+    msg = _msg.decode()
+    print(f'Topic: "{topic}" Message: "{msg}" Retained: {retained}')
+    if topic == f'{MQTT_TOPIC_PREFIX}/echo':
+        asyncio.create_task(echo(msg))
+
+async def main(client):
+    try:
+        await client.connect()
+    except OSError:
+        print('Status: Connection Failed')
+        return
+    n = 0
+    while True:
+        await asyncio.sleep(5)
+        # print('publish', n)
+        # await client.publish(TOPIC_PREFIX, '{} repubs: {} outages: {}'.format(n, client.REPUB_COUNT, outages), qos = 1)
+        n += 1
+
+config['subs_cb'] = on_message
+config['wifi_coro'] = handle_wifi
+config['will'] = (MQTT_TOPIC_PREFIX, 'GOODBYE', False, 0)
+config['connect_coro'] = handle_connection
+config['keepalive'] = 120
+
+MQTTClient.DEBUG = True
+client = MQTTClient(config)
+
+try:
+    asyncio.run(main(client))
+finally:  # Prevent LmacRxBlk:1 errors.
+    client.close()
+    asyncio.new_event_loop()
+
+"""
 while True:
     (year, month, day, hour, minute, second, weekday, _) = time.localtime()[:8]
     now_fmt = '{:02d}:{:02d}'.format(hour, minute)
@@ -38,3 +99,4 @@ while True:
     time.sleep(2)
     display.hscroll(-1)
     time.sleep(1)
+"""
